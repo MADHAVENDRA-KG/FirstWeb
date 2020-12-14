c242460540f6c4a7be9a3da6513122bf84fe9aa2
MADHAVENDRA-KG <66855883+MADHAVENDRA-KG@users.noreply.github.com>
2020-12-14 10:31:20 +0530

    First

---
+++ YelpCamp-40d8f37a93d140b53a59ee847e3bed3aeb94ecc4/seeds/index.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+const cities = require('./cities');
+const { places, descriptors } = require('./seedHelpers');
+const Campground = require('../models/campground');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+const sample = array => array[Math.floor(Math.random() * array.length)];
+
+
+const seedDB = async () => {
+    await Campground.deleteMany({});
+    for (let i = 0; i < 300; i++) {
+        const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
+        const camp = new Campground({
+            author: '5fd0401df4a2ab2230d89fa9',
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: [
+                {
+                    url: 'https://res.cloudinary.com/dboflwrld/image/upload/v1607496224/YelpCamp/a2iqdckwlal3a9am4kqg.png',
+                    filename: 'YelpCamp/a2iqdckwlal3a9am4kqg'
+                },
+                {
+                    url: 'https://res.cloudinary.com/dboflwrld/image/upload/v1607509249/YelpCamp/fnz42ks6tms5jddaqzfu.jpg',
+                    filename: 'YelpCamp/fnz42ks6tms5jddaqzfu'
+                }
+            ],
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
+            price,
+            geometry: {
+                type: "Point",
+                coordinates: [
+                    cities[random1000].longitude,
+                    cities[random1000].latitude
+                ]
+            }
+        })
+        await camp.save();
+    }
+}
+
+seedDB().then(() => {
+    mongoose.connection.close();
+})
